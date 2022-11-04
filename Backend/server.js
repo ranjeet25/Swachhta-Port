109d31e80a9258c9b141a78ac28110218dedae01
Ranjeet Saw <sawranjeet550@gmail.com>
2022-11-04 23:48:16 +0530

    modified files

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(express.static("../Pages"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
